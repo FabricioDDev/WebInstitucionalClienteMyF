595d921d0e6db4d5b4e8a861d174135f880f11ee
Fabricio Damian Di Paola <111092778+FabricioDDev@users.noreply.github.com>
2026-06-13 12:23:41 -0300

    services-visualprototype-created

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     const navToggle = document.getElementById("nav-toggle");
     const navMenu = document.getElementById("nav-menu");
+    const filterButtons = document.querySelectorAll(".filter-btn");
+    const cards = document.querySelectorAll(".service-overlay-card");
 
     // Validamos que los elementos existan en la página para evitar errores
     if (navToggle && navMenu) {
@@ -17,4 +19,22 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         });
     }
+        filterButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            // Remover estado activo de todos los botones y ponerlo en el clickeado
+            filterButtons.forEach(btn => btn.classList.remove("active"));
+            button.classList.add("active");
+
+            const selectedCategory = button.getAttribute("data-category");
+
+            // Mostrar u ocultar tarjetas según el data-cat
+            cards.forEach(card => {
+                if (card.getAttribute("data-cat") === selectedCategory) {
+                    card.classList.remove("hidden");
+                } else {
+                    card.classList.add("hidden");
+                }
+            });
+        });
+    });
 });
